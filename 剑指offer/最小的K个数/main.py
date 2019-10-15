1facd48585614eab9d693baf340c707153094962
JeffyLu <JeffyLuuu@qq.com>
2019-10-15 22:10:57 +0800

    using heap

--- 剑指offer/最小的K个数/main.py
+++ 剑指offer/最小的K个数/main.py
@@ -1,55 +1,27 @@
 # -*- coding:utf-8 -*-
 
 
-class MergeSort:
-
-    def _merge(self, arr1, arr2):
-        arr = []
-        while len(arr1) != 0 and len(arr2) != 0:
-            if arr1[0] > arr2[0]:
-                arr.append(arr2.pop(0))
-            else:
-                arr.append(arr1.pop(0))
-        if len(arr1) == 0:
-            arr.extend(arr2)
-        else:
-            arr.extend(arr1)
-        return arr
-
-    def _sort(self, arr):
-        if len(arr) <= 1:
-            return arr
-        else:
-            mid = len(arr) // 2
-            arr1 = self._sort(arr[:mid])
-            arr2 = self._sort(arr[mid:])
-            return self._merge(arr1, arr2)
-
-    def __call__(self, arr):
-        return self._sort(arr)
-
-
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]
-    left = [i for i in arr if i < pivot]
-    mid = [i for i in arr if i == pivot]
-    right = [i for i in arr if i > pivot]
-    return quick_sort(left) + mid + quick_sort(right)
-
-
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
-    return arr
-
-
 class Solution:
 
+    def _heapify(self, arr, length, last):
+        smallest = last
+        left = last * 2 + 1
+        right = last * 2 + 2
+        if left < length and arr[left] < arr[smallest]:
+            smallest = left
+        if right < length and arr[right] < arr[smallest]:
+            smallest = right
+        if smallest != last:
+            arr[smallest], arr[last] = arr[last], arr[smallest]
+            self._heapify(arr, length, smallest)
+
     def GetLeastNumbers_Solution(self, tinput, k):
-        if k > len(tinput):
+        if k > len(tinput) or k == 0:
             return []
-        return bubble_sort(tinput)[:k]
+        length = len(tinput)
+        for i in range(int(length/2)-1, -1, -1):
+            self._heapify(tinput, length, i)
+        for i in range(length-1, length-1-k, -1):
+            tinput[0], tinput[i] = tinput[i], tinput[0]
+            self._heapify(tinput, i, 0)
+        return tinput[-k:]
