2b18b95f6dd181d0cebd948515cb380ac657bfa7
JeffyLu <JeffyLuuu@qq.com>
2019-10-16 09:57:04 +0800

    优化

--- 剑指offer/数字在排序数组中出现的次数/main.py
+++ 剑指offer/数字在排序数组中出现的次数/main.py
@@ -3,41 +3,16 @@
 
 class Solution:
 
-    def GetNumberOfK(self, data, k):
+    def insert_idx(self, data, val):
         left = 0
         right = len(data) - 1
         while left <= right:
             mid = (right + left) // 2
-            if data[mid] == k:
-                break
-            elif data[mid] > k:
-                right = mid - 1
-            else:
-                left = mid + 1
-        if left > right:
-            return 0
-        cnt = 1
-        l = mid - 1
-        r = mid + 1
-        lend = False
-        rend = False
-        while True:
-            if lend and rend:
-                break
-            if l >= 0:
-                if data[l] == k:
-                    cnt += 1
-                else:
-                    lend = True
-            else:
-                lend = True
-            if r < len(data):
-                if data[r] == k:
-                    cnt += 1
-                else:
-                    rend = True
-            else:
-                rend = True
-            l -= 1
-            r += 1
-        return cnt
+            if data[mid] < val:
+                left += 1
+            elif data[mid] > val:
+                right -= 1
+            return left
+
+    def GetNumberOfK(self, data, k):
+        return self.insert_idx(data, k+0.5) - self.insert_idx(data, k-0.5)
