5ee73d91f979805d83b481df03ac6e201b4ccf29
JeffyLu <JeffyLuuu@qq.com>
2019-10-27 10:59:32 +0800

    优化

--- 剑指offer/连续子数组的最大和/main.py
+++ 剑指offer/连续子数组的最大和/main.py
@@ -4,11 +4,19 @@
 class Solution:
 
     def FindGreatestSumOfSubArray(self, array):
-        max_sum = max(array)
-        for i in xrange(len(array)):
-            tmp = array[i]
-            for j in xrange(i+1, len(array)):
-                tmp += array[j]
-                if tmp > max_sum:
-                    max_sum = tmp
-        return max_sum
+        i = 0
+        res = array[0]
+        while i < len(array):
+            tmp_res = 0
+            j = i
+            while j < len(array):
+                tmp_res += array[j]
+                if tmp_res > res:
+                    res = tmp_res
+                j += 1
+                if tmp_res < 0:
+                    i = j
+                    break
+            if j == len(array):
+                break
+        return res
