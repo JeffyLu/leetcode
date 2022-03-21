8ee2c3d61a0182de1e18d0b2dad482566489c686
JeffyLu <JeffyLuuu@qq.com>
2022-03-21 22:51:26 +0800

    Permutation-Sequence

---
+++ leetcode/0060.Permutation-Sequence/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"math"
+)
+
+func main() {
+	cases := []struct {
+		n   int
+		k   int
+		res string
+	}{
+		{3, 1, "123"},
+		{3, 2, "132"},
+		{3, 3, "213"},
+		{3, 6, "321"},
+		{4, 9, "2314"},
+		{9, 30000, "179586432"},
+	}
+	for _, c := range cases {
+		log.Printf("%+v", c)
+		if res := getPermutation(c.n, c.k); res != c.res {
+			log.Fatalf("expect: %s, got: %s", c.res, res)
+		}
+	}
+}
+
+func getPermutation(n int, k int) string {
+	var nums []string
+	numStrs := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	for i := 0; i < n; i++ {
+		nums = append(nums, numStrs[i])
+	}
+	return selectNum(nums, k)
+}
+
+func selectNum(nums []string, k int) string {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	total := 1
+	for i := 1; i <= len(nums); i++ {
+		total *= i
+	}
+	group := total / len(nums)
+
+	selected := int(math.Ceil(float64(k)/float64(group))) - 1
+	var next []string
+	for _, s := range nums {
+		if s != nums[selected] {
+			next = append(next, s)
+		}
+	}
+	return nums[selected] + selectNum(next, (k-1)%group+1)
+}
